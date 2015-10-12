692115208e07497ad1cfaee60ed0082ddbcc19ba
thydeyx <553865290@qq.com>
2015-10-12 13:47:16 +0800

    add

---
+++ Regular Expression Matching.py
@@ -0,0 +1,10 @@
+import re
+class Solution(object):
+    def isMatch(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: bool
+        """
+        return bool(re.match(p+'$',s))
+        
