31c795e056b86d4e41cdef83d61536a2ba15adce
thydeyx <553865290@qq.com>
2016-10-19 08:58:30 +0800

    test

---
+++ Wiggle_Subsequence.py
@@ -0,0 +1,28 @@
+# -*- coding:utf-8 -*-
+
+class Solution(object):
+	def wiggleMaxLength(self, nums):
+		"""
+		:type nums: List[int]
+		:rtype: int
+		"""
+		n = len(nums)
+		ret = 0
+		dp = [[0,0] for x in range(n)]
+		print dp
+		for i in range(n):
+			for j in range(i):
+				if nums[i] - nums[j] > 0:
+					dp[i][0] = max(dp[j][1] + 1, dp[i][0])
+				elif nums[j] - nums[i] < 0:
+					dp[i][1] = max(dp[j][0] + 1, dp[i][1])
+
+			print dp[i][1], dp[i][0]
+			ret = max(ret, max(dp[i][0], dp[i][1]))
+
+		return ret
+
+if __name__ == "__main__":
+	s = Solution()
+	nums = [1,7,4,9,2,5]
+	print s.wiggleMaxLength(nums)
