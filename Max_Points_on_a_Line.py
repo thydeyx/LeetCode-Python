1a38afdfe583941f440c68c34f054639a5447ac4
thydeyx <553865290@qq.com>
2016-10-24 15:53:31 +0800

    第三版提交

--- Max_Points_on_a_Line.py
+++ Max_Points_on_a_Line.py
@@ -26,6 +26,7 @@ class Solution(object):
 		ret = 0
 		dict_x = {}
 		for p1 in points:
+			c = 0
 			dict_k = {}
 			maxK = 0
 			for p2 in points:
@@ -37,10 +38,16 @@ class Solution(object):
 					dict_k[k] = now_k + 1
 					if now_k + 1 > maxK:
 						maxK = now_k + 1
+				if p1.x == p2.x and p1.y == p2.y:
+					c += 1
+
+			for key in dict_k.iterkeys():
+				dict_k[key] += c
+				if dict_k[key] > ret:
+					ret = dict_k[key]
+
 			now_x = dict_x.get(p1.x, 0)
 			dict_x[p1.x] = now_x + 1
-			if maxK > ret:
-				ret = maxK
 			if now_x + 1 > ret:
 				ret = now_x + 1
 
@@ -50,9 +57,9 @@ class Solution(object):
 if __name__ == "__main__":
 	s = Solution()
 	point1 = Point(1, 1)
-	point2 = Point(0, 0)
+	point2 = Point(1, 1)
 	point3 = Point(2, 2)
-	point4 = Point(1, 2)
-#points = [point1, point2, point3, point4]
-	points = [point2]
+	point4 = Point(2, 2)
+	points = [point1, point2, point3, point4]
+	#points = [point2]
 	print s.maxPoints(points)
