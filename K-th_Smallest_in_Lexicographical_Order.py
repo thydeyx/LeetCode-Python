44eb545da75081ff962f1f7881a19840249f354d
thydeyx <553865290@qq.com>
2016-10-24 10:22:29 +0800

    增加了求第K个字典序数字的注释

--- K-th_Smallest_in_Lexicographical_Order.py
+++ K-th_Smallest_in_Lexicographical_Order.py
@@ -2,6 +2,13 @@
 
 import sys
 
+"""
+这个问题可以根据在i,i+1之间的数字个数计算
+这个个数可以通过如下的代码进行计算
+while interval[0] <= n:
+	count += min(interval[1], n + 1) - interval[0]
+	interval = [10 * interval[0], 10 * interval[1]]
+"""
 class Solution(object):
 	def findKthNumber(self, n, k):
 		ret = 1
