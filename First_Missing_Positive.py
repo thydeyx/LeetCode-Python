c33449d726c726b470c9f3071cc280384c54653b
thydeyx <553865290@qq.com>
2016-10-26 22:10:30 +0800

    无序数组中第一个消失的正整数，已加注释

--- First_Missing_Positive.py
+++ First_Missing_Positive.py
@@ -5,11 +5,14 @@
 # Create Date : 16-10-26 21:21:41
 #   File Name : First_Missing_Positive.py
 #        Desc :
-
-
+"""
+因为这个数不会超过N，所以把每个数交换到本来的位置上,扫描一边就好了
+"""
 class Solution(object):
 	def firstMissingPositive(self, nums):
 		n = len(nums)
+		if n == 0:
+			return 1
 		i = 0
 		while i < n:
 			if (nums[i] <= 0 or nums[i] > n) and i < n:
@@ -22,6 +25,8 @@ class Solution(object):
 		for i in range(n):
 			if nums[i] - 1 != i:
 				return i + 1
+		
+		return n + 1
 		
 
 if __name__ == "__main__":
