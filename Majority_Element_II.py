727d29b3ba0117ea5f08b23e1e18bf0f9d924c70
thydeyx <553865290@qq.com>
2016-10-27 21:04:53 +0800

    第二版提交

--- Majority_Element_II.py
+++ Majority_Element_II.py
@@ -11,20 +11,31 @@ class Solution(object):
 		ret = []
 		if n == 0:
 			return ret
+		if n == 1:
+			ret.append(nums[0])
+			return ret
+		if n == 2:
+			if nums[0] == nums[1]:
+				ret.append(nums[0])
+				return ret
+			else:
+				ret.append(nums[0])
+				ret.append(nums[1])
+				return ret
 		
 		number0, number1 = nums[0], nums[1]
 		count0, count1 = 1, 1
 		for i in range(2, n):
-			if count0 == 0:
+			if nums[i] == number0:
+				count0 += 1
+			elif nums[i] == number1:
+				count1 += 1
+			elif count0 == 0:
 				number0 = nums[i]
 				count0 = 1
 			elif count1 == 0:
 				number1 = nums[i]
 				count1 = 1
-			elif nums[i] == number0:
-				count0 += 1
-			elif nums[i] == number1:
-				count1 += 1
 			else:
 				count0 -= 1
 				count1 -= 1
@@ -47,4 +58,5 @@ class Solution(object):
 if __name__ == "__main__":
 	s = Solution()
 	nums = [1,1,1,2,2,2,2,3,3,3,3]
+	nums = [8,8,7,7,7]
 	print s.majorityElement(nums)
