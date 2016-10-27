7b4fb98d35dcdf51aa042f16170ee219ed00d983
thydeyx <553865290@qq.com>
2016-10-27 09:10:21 +0800

    第一版提交

---
+++ Two_Sum_II_Input_array_is_sorted.py
@@ -0,0 +1,26 @@
+# -*- coding:utf-8 -*-
+#
+#      Author : TangHanYi
+#      E-mail : thydeyx@163.com
+# Create Date : 16-10-27 09:04:15
+#   File Name : Two_Sum_II_Input_array_is_sorted.py
+#        Desc :
+
+class Solution(object):
+	def twoSum(self, numbers, target):
+		r = len(numbers) - 1
+		l = 0
+		while l < r:
+			if numbers[l] + numbers[r] < target:
+				l += 1
+			elif numbers[l] + numbers[r] > target:
+				r -= 1
+			else:
+				return [l, r]
+
+
+if __name__ == "__main__":
+	s = Solution()
+	numbers = [2,7,11,15]
+	target = 9
+	print s.twoSum(numbers, target)
