ba5a5cd8aceb8bb222293746216d79eebbdee9b5
thydeyx <553865290@qq.com>
2016-10-27 21:08:36 +0800

    找超过n/3的数字

--- Majority_Element_II.py
+++ Majority_Element_II.py
@@ -5,6 +5,9 @@
 # Create Date : 16-10-27 20:37:48
 #   File Name : Majority_Element_II.py
 #        Desc :
+"""
+思路同灌水贴王
+"""
 class Solution(object):
 	def majorityElement(self, nums):
 		n = len(nums)
@@ -23,9 +26,9 @@ class Solution(object):
 				ret.append(nums[1])
 				return ret
 		
-		number0, number1 = nums[0], nums[1]
-		count0, count1 = 1, 1
-		for i in range(2, n):
+		number0, number1 = nums[0], nums[0]
+		count0, count1 = 0, 0
+		for i in range(n):
 			if nums[i] == number0:
 				count0 += 1
 			elif nums[i] == number1:
