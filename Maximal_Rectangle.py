f1706bfd3296b93f45940f74effc1c08c7381c47
thydeyx <553865290@qq.com>
2016-11-02 15:58:34 +0800

    最大子矩阵，方程需要给出解释

---
+++ Maximal_Rectangle.py
@@ -0,0 +1,18 @@
+# -*- coding:utf-8 -*-
+#
+#        Author : TangHanYi
+#        E-mail : thydeyx@163.com
+#   Create Date : 2016-11-01 10:22:23 PM
+# Last modified : 2016-11-02 10:00:05 AM
+#     File Name : Maximal_Rectangle.py
+#          Desc :
+class Solution(object):
+	def maximalRectangle(self, matrix):
+		m = len()
+		pass
+
+
+if __name__ == "__main__":
+	s = Solution()
+	matrix = [[1,0,1,0,0],[1,0,1,1,1],[1,1,1,1,1],[1,0,0,1,0]]
+	s.maximalRectangle(matrix)
