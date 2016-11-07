ef21afcbe50b5c32eb6279cc53a8afd0eb728ce3
thydeyx <553865290@qq.com>
2016-11-07 15:53:03 +0800

    随机取数，已加注释

--- Linked_List_Random_Node.py
+++ Linked_List_Random_Node.py
@@ -3,13 +3,15 @@
 #        Author : TangHanYi
 #        E-mail : thydeyx@163.com
 #   Create Date : 2016-11-07 03:31:51 PM
-# Last modified : 2016-11-07 03:47:40 PM
+# Last modified : 2016-11-07 03:52:52 PM
 #     File Name : Linked_List_Random_Node.py
 #          Desc :
 
 import sys
 import random
-
+"""
+1/(n+1)
+"""
 class ListNode(object):
     def __init__(self, x):
         self.val = x
