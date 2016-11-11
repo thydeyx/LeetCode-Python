75bde1970bd6c9b883f6dbe524a17961017dd884
thydeyx <553865290@qq.com>
2016-11-11 14:03:31 +0800

    第二版提交

--- Find_All_Duplicates_in_an_Array.py
+++ Find_All_Duplicates_in_an_Array.py
@@ -3,7 +3,7 @@
 #        Author : TangHanYi
 #        E-mail : thydeyx@163.com
 #   Create Date : 2016-11-11 01:35:06 PM
-# Last modified : 2016-11-11 01:47:58 PM
+# Last modified : 2016-11-11 02:03:22 PM
 #     File Name : Find_All_Duplicates_in_an_Array.py
 #          Desc :
 class Solution(object):
@@ -12,13 +12,11 @@ class Solution(object):
 		ret = []
 
 		for i in range(n):
-			j = i
-			k = nums[i]
-			while k - 1 != j:
-				if k == nums[k - 1] and j >= i:
-					ret.append(k)
-				j = k - 1
-				nums[k - 1], k = k, nums[k - 1]
+			index = abs(nums[i]) - 1
+			if nums[index] < 0:
+				ret.append(index + 1)
+
+			nums[index] = - nums[index]
 		
 		return ret
 
