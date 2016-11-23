cf614e0444eb7ab75cd4f5f7e6e86c1c334650fa
thydeyx <553865290@qq.com>
2016-11-23 23:56:48 +0800

    等差数列子序列个数

--- Arithmetic_Slices.py
+++ Arithmetic_Slices.py
@@ -3,9 +3,17 @@
 #        Author : TangHanYi
 #        E-mail : thydeyx@163.com
 #   Create Date : 2016-11-23 09:18:03 PM
-# Last modified : 2016-11-23 11:54:53 PM
+# Last modified : 2016-11-23 11:56:45 PM
 #     File Name : Arithmetic_Slices.py
 #          Desc :
+"""
+数组    等差数列的数目    与上一数组的等差数列数目比较
+1 2 3			 1     1 - 0 = 1
+1 2 3 4			 3     3 - 1 = 2
+1 2 3 4 5		 6     6 - 3 = 3
+1 2 3 4 5 6		 10    10 - 6 = 4
+1 2 3 4 5 6 7    15    15 - 10 = 5
+"""
 class Solution(object):
 	def numberOfArithmeticSlices(self, A):
 		addend = 0
