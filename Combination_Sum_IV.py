0bb2d614d7ade9308eded76429080595dd36d0e6
thydeyx <553865290@qq.com>
2016-11-27 00:23:53 +0800

    组合成目标数

--- Combination_Sum_IV.py
+++ Combination_Sum_IV.py
@@ -3,26 +3,20 @@
 #        Author : TangHanYi
 #        E-mail : thydeyx@163.com
 #   Create Date : 2016-11-26 11:34:16 PM
-# Last modified : 2016-11-26 11:39:00 PM
+# Last modified : 2016-11-27 12:23:50 AM
 #     File Name : Combination_Sum_IV.py
 #          Desc :
 class Solution(object):
-	def __init__(self):
-		self.ret = 0
-
-	def dfs(self, nums, target, n):
-		if target == 0:
-			self.ret += 1
-			return
-
-		for i in range(n):
-			if target >= nums[i]:
-				self.dfs(nums, target - nums[i], n)
-
-
 	def combinationSum4(self, nums, target):
-		self.dfs(nums, target, len(nums))
-		return self.ret
+		dp = [0 for i in range(target + 1)]
+		dp[0] = 1
+		n = len(nums)
+		for i in range(target + 1):
+			for j in range(n):
+				if i >= nums[j]:
+					dp[i] = dp[i] + dp[i - nums[j]]
+
+		return dp[target]
 
 
 if __name__ == "__main__":
