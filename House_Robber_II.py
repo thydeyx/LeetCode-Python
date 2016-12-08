b6cc6c91d32f846294ea14bf2f613a9ce571cd76
thydeyx <553865290@qq.com>
2016-12-08 23:33:04 +0800

    动态规划

---
+++ House_Robber_II.py
@@ -0,0 +1,31 @@
+# -*- coding:utf-8 -*-
+#
+#        Author : TangHanYi
+#        E-mail : thydeyx@163.com
+#   Create Date : 2016-12-07 10:58:56 PM
+# Last modified : 2016-12-08 11:32:35 PM
+#     File Name : House_Robber_II.py
+#          Desc :
+class Solution(object):
+	def rob(self, nums):
+		n = len(nums)
+		dp = [[0,0] for i in range(n)]
+		#dp[0][1] = nums[0]
+		for i in range(1, n):
+			dp[i][0] = max(dp[i - 1][0], dp[i - 1][1])
+			dp[i][1] = max(dp[i - 1][0], dp[i][1]) + nums[i]
+
+		ret1 = max(dp[n - 1][0], dp[n - 1][1])
+		dp = [[0,0] for i in range(n)]
+		dp[0][1] = nums[0]
+		dp[1][0] = nums[0]
+		for i in range(2, n):
+			dp[i][0] = max(dp[i - 1][0], dp[i - 1][1])
+			dp[i][1] = max(dp[i - 1][0], dp[i][1]) + nums[i]
+		return max(ret1, dp[n - 1][0])
+
+
+if __name__ == "__main__":
+	s = Solution()
+	nums = [1,2,3,4,5,6]
+	print s.rob(nums)
---
+++ Super_Ugly_Number.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*-
+#
+#        Author : TangHanYi
+#        E-mail : thydeyx@163.com
+#   Create Date : 2016-12-05 02:44:14 PM
+# Last modified : 2016-12-05 02:44:18 PM
+#     File Name : Super_Ugly_Number.py
+#          Desc :
+
+
+if __name__ == "__main__":
+	s = Solution()
