6118351bbc720c82b6557142ae66c160fe0577ce
thydeyx <553865290@qq.com>
2016-12-12 16:50:12 +0800

    水流是否能流入不同的海洋

--- Pacific_Atlantic_Water_Flow.py
+++ Pacific_Atlantic_Water_Flow.py
@@ -3,19 +3,19 @@
 #        Author : TangHanYi
 #        E-mail : thydeyx@163.com
 #   Create Date : 2016-12-12 03:59:52 PM
-# Last modified : 2016-12-12 04:39:46 PM
+# Last modified : 2016-12-12 04:50:09 PM
 #     File Name : Pacific_Atlantic_Water_Flow.py
 #          Desc :
 import Queue
 class Solution(object):
 
-	def findPath(self, i, j, ret, matrix, n, m):
+	def findPath(self, q, ret, matrix, n, m):
 		
-		q = Queue.Queue()
+		#q = Queue.Queue()
 		dirX = [-1, 0, 1, 0]
 		dirY = [0, -1, 0, 1]
-		q.put((i,j))
-		ret[i][j] = True
+		#q.put((i,j))
+		#ret[i][j] = True
 		while q.empty() == False:
 			x, y = q.get()
 			for d in range(4):
@@ -39,17 +39,29 @@ class Solution(object):
 			return ret
 		Pacific = []
 		Atlantic = []
+		pQueue = Queue.Queue()
+		aQueue = Queue.Queue()
 		for i in range(n):
 			Pacific.append([False for j in range(m)]) 
 			Atlantic.append([False for j in range(m)])
 
 		for i in range(n):
-			self.findPath(i, 0, Pacific, matrix, n, m)
-			self.findPath(i, m - 1, Atlantic, matrix, n, m)
+			pQueue.put(i, 0)
+			q.Queue.put(i, m - 1)
+			Pacific[i][0] = True
+			Atlantic[i][m - 1] = True
+			#self.findPath(i, 0, Pacific, matrix, n, m)
+			#self.findPath(i, m - 1, Atlantic, matrix, n, m)
 
 		for i in range(m):
-			self.findPath(0, i, Pacific, matrix, n, m)
-			self.findPath(n - 1, i, Atlantic, matrix, n, m)
+			pQueue.put(0, i)
+			q.Queue.put(n - 1, i)
+			Pacific[0][i] = True
+			Atlantic[n - 1][i] = True
+			#self.findPath(0, i, Pacific, matrix, n, m)
+			#self.findPath(n - 1, i, Atlantic, matrix, n, m)
+		self.findPath(pQueue, Pacific, matrix, n, m)
+		self.findPath(aQueue, Pacific, matrix, n, m)
 
 		for i in range(n):
 			for j in range(m):
