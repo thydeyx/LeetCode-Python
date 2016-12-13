3b3bdc5b3c01cdda9be24d9b57893f5fa92125be
thydeyx <553865290@qq.com>
2016-12-13 17:40:52 +0800

    链表排序,注释

--- Insertion_Sort_List.py
+++ Insertion_Sort_List.py
@@ -3,11 +3,12 @@
 #        Author : TangHanYi
 #        E-mail : thydeyx@163.com
 #   Create Date : 2016-12-13 02:47:07 PM
-# Last modified : 2016-12-13 05:23:28 PM
+# Last modified : 2016-12-13 05:40:43 PM
 #     File Name : Insertion_Sort_List.py
 #          Desc :
-
-
+"""
+简单优化  当前递增序列则跳过不更改，当前序列开始递减，则把递减值插入之前的已递增序列中
+"""
 # Definition for singly-linked list.
 class ListNode(object):
     def __init__(self, x):
